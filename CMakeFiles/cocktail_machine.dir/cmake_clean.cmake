ec5d6d7ad69c9b3408a88db858a2cc4c9a5967b5
Ethan Lavis <elavis00@outlook.com>
2024-02-29 23:20:22 +0000

    moved code to a cmake stucture

---
+++ CMakeFiles/cocktail_machine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cocktail_machine.dir/main.cpp.o"
+  "cocktail_machine"
+  "cocktail_machine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cocktail_machine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
