75554be7d0d13e562d0931d45bb3b9413bef9a41
Roy16Keane <rsyangu16@gmail.com>
2024-03-20 20:52:11 +0000

    light gate class modified to initialize the pin and get the state

--- Include/LightGate.h
+++ Include/LightGate.h
@@ -1,19 +1,25 @@
 #ifndef LIGHT_GATE_H
 #define LIGHT_GATE_H
-
 #include <iostream>
+
+
 
 using namespace std;
 
 class LightGate{
-	int pin;
-	int state;
-	
+public:
+    LightGate(int switchPin); 
+	bool initialize();
+	bool returnState();
+
 	int getPin();
 	void setPin(int p);
 	int getState();
 	void setState(int s);
-	void changeState();
+	
+private:
+    int lightGateSwitchPin;
+
 	
 };
 #endif
--- src/LightgateClass.cpp
+++ src/LightgateClass.cpp
@@ -1,26 +1,47 @@
 #include "../Include/LightGate.h"
 #include <iostream>
+#include <pigpio>
 
 using namespace std;
 
+LightGate::LightGate(int pin) : lightGateSwitchPin(pin) {}
+
+
 int LightGate::getPin(){
-	return this->pin;
+	return lightGateSwitchPin;
 }
 void LightGate::setPin(int p){
-	this->pin = p;
+	lightGateSwitchPin = p;
 }
 int LightGate::getState(){
-	return this->state;
+	return state;
 }
 void LightGate::setState(int s){
-	this->state = s;
+	state = s;
 }
-void LightGate::changeState(){
-	int currentState = this->getState();
-	if(currentState == 0){
-		this->setState(1);
+
+//initialize the light gate gpio pin 
+bool LightGate::initialize(){
+	if(gpioInitialise() < 0) {
+        cout<<"Not initialized.\n"
+		return false;
+	}else{
+		gpioSetMode( lightGateSwitchPin,PI_INPUT);// Set the GPIO pin as an input
+	return true;
+
 	}
-	else{
-		this->setState(0);
+}
+ // Read the state of the GPIO pin (HIGH/on or Low/off) prints it for confirmation then  return it
+
+bool LightGate::returnState(){
+
+	if(gpioRead(lightGateSwitchPin));{
+		cout<<"light gate switch is on.\n";
+		return true;
+	}else{
+		cout<<"Light gate switch is off.\n";
+	return false;
 	}
+	
+	
 }
