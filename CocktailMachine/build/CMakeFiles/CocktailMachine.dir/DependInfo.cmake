718ab9d196d821fabc4452c780c3b040f96bd877
CymruLavis <elavis00@outlook.com>
2024-04-14 16:14:36 +0100

    brinigng local repo up to speed

---
+++ CocktailMachine/build/CMakeFiles/CocktailMachine.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/admin/Desktop/cocktail_machine/CocktailMachine/build/CocktailMachine_autogen/mocs_compilation.cpp" "CMakeFiles/CocktailMachine.dir/CocktailMachine_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/CocktailMachine.dir/CocktailMachine_autogen/mocs_compilation.cpp.o.d"
+  "/home/admin/Desktop/cocktail_machine/CocktailMachine/UnitTests/PIRUnitTest.cpp" "CMakeFiles/CocktailMachine.dir/UnitTests/PIRUnitTest.cpp.o" "gcc" "CMakeFiles/CocktailMachine.dir/UnitTests/PIRUnitTest.cpp.o.d"
+  "/home/admin/Desktop/cocktail_machine/CocktailMachine/main.cpp" "CMakeFiles/CocktailMachine.dir/main.cpp.o" "gcc" "CMakeFiles/CocktailMachine.dir/main.cpp.o.d"
+  "/home/admin/Desktop/cocktail_machine/CocktailMachine/mainwindow.cpp" "CMakeFiles/CocktailMachine.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/CocktailMachine.dir/mainwindow.cpp.o.d"
+  "/home/admin/Desktop/cocktail_machine/CocktailMachine/src/DataClass.cpp" "CMakeFiles/CocktailMachine.dir/src/DataClass.cpp.o" "gcc" "CMakeFiles/CocktailMachine.dir/src/DataClass.cpp.o.d"
+  "/home/admin/Desktop/cocktail_machine/CocktailMachine/src/IngredientClass.cpp" "CMakeFiles/CocktailMachine.dir/src/IngredientClass.cpp.o" "gcc" "CMakeFiles/CocktailMachine.dir/src/IngredientClass.cpp.o.d"
+  "/home/admin/Desktop/cocktail_machine/CocktailMachine/src/IngredientIndexClass.cpp" "CMakeFiles/CocktailMachine.dir/src/IngredientIndexClass.cpp.o" "gcc" "CMakeFiles/CocktailMachine.dir/src/IngredientIndexClass.cpp.o.d"
+  "/home/admin/Desktop/cocktail_machine/CocktailMachine/src/LimitSwitchClass.cpp" "CMakeFiles/CocktailMachine.dir/src/LimitSwitchClass.cpp.o" "gcc" "CMakeFiles/CocktailMachine.dir/src/LimitSwitchClass.cpp.o.d"
+  "/home/admin/Desktop/cocktail_machine/CocktailMachine/src/MotorClass.cpp" "CMakeFiles/CocktailMachine.dir/src/MotorClass.cpp.o" "gcc" "CMakeFiles/CocktailMachine.dir/src/MotorClass.cpp.o.d"
+  "/home/admin/Desktop/cocktail_machine/CocktailMachine/src/SystemConfig.cpp" "CMakeFiles/CocktailMachine.dir/src/SystemConfig.cpp.o" "gcc" "CMakeFiles/CocktailMachine.dir/src/SystemConfig.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- CocktailMachine/main.cpp
+++ CocktailMachine/main.cpp
@@ -1,52 +1,73 @@
 #include "mainwindow.h"
 #include <iostream>
 #include <thread>
+#include <typeinfo>
 #include <QApplication>
+#include <pigpio.h>
+#include <vector>
+#include <QSplashScreen>
+#include <QTimer>
 
 #include "Include/Data.h"
-#include "Include/SystemConfig.h"
+// #include "Include/SystemConfig.h"
 #include "Include/Motor.h"
 #include "Include/LimitSwitch.h"
 
+
 // #include "UnitTests/LimitSwitchUnitTest.h"
 // #include "UnitTests/PIRUnitTest.h"
-// #include "UnitTests/DataBaseUnitTest.h"
+//#include "UnitTests/DataBaseUnitTest.h"
 
 using namespace std;
 
-LimitSwitch* PIR_sensor = new LimitSwitch(26); //physical pin 37
-LimitSwitch* upper_switch = new LimitSwitch(4); //
-LimitSwitch* lower_switch = new LimitSwitch(27); //
-LimitSwitch* calibration_switch = new LimitSwitch(22); //
+// LimitSwitch* PIR_sensor = new LimitSwitch(26); //physical pin 37
+// LimitSwitch* upper_switch = new LimitSwitch(4); //
+// LimitSwitch* lower_switch = new LimitSwitch(27); //
+// LimitSwitch* calibration_switch = new LimitSwitch(22); //
 
-Motor* carousel_motor = new Motor(0, 19,3,2,1.8);
-Motor* riser_motor = new Motor(20,18,21,16,1.8);
+// Motor* carousel_motor = new Motor(0, 19,3,2,1.8);
+// Motor* riser_motor = new Motor(20,18,21,16,1.8);
 
-
-
-void initializePins(){
-	//PIR Sensor
-	gpioSetMode(PIR_sensor->getPin(), PI_INPUT);
-
-	//Limit switches
-	gpioSetMode(upper_switch->getPin(), PI_INPUT);
-	gpioSetMode(lower_switch->getPin(), PI_INPUT);
-	gpioSetMode(calibration_switch->getPin(), PI_INPUT);
-
-	//Light Gate
-
-	//Motors
-	gpioSetMode(carousel_motor->getDirPin(), PI_OUTPUT);
+// Data* myData = new Data();
 
 
+// void initializePins(){
+// 	//PIR Sensor
+// 	gpioSetMode(PIR_sensor->getPin(), PI_INPUT);
 
-}
+// 	//Limit switches
+// 	gpioSetMode(upper_switch->getPin(), PI_INPUT);
+// 	gpioSetMode(lower_switch->getPin(), PI_INPUT);
+// 	gpioSetMode(calibration_switch->getPin(), PI_INPUT);
+
+// 	//Light Gate
+
+// 	//Motors
+// 	gpioSetMode(carousel_motor->getDirPin(), PI_OUTPUT);
+
+
+
+// }
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    
+	QApplication a(argc, argv);
+	QPixmap pixmap("../splash.png");
+	pixmap = pixmap.scaled(480, 320, Qt::KeepAspectRatio);
+	QSplashScreen *splash = new QSplashScreen;
+	splash->setPixmap(pixmap);
+	splash->show();
+
+	//a.processEvents();
+
     MainWindow w;
-	w.setWindowState(Qt::WindowFullScreen);
-    w.show();
+	//w.setWindowState(Qt::WindowFullScreen);
+
+	QTimer::singleShot(2500, splash, SLOT(close()));
+	QTimer::singleShot(2500, &w, SLOT(show()));
+
+    //w.show();
+	//splash.finish(&w);
     return a.exec();
 }
