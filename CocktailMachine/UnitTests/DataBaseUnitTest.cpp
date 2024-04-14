5c88acbb010d7914a477f0211a12e69eb381d6cf
CymruLavis <117460546+CymruLavis@users.noreply.github.com>
2024-04-14 21:06:45 +0100

    Update DataBaseUnitTest.cpp

--- CocktailMachine/UnitTests/DataBaseUnitTest.cpp
+++ CocktailMachine/UnitTests/DataBaseUnitTest.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "../Include/Data.h"
+// #include "../Include/SystemConfig.h"
 #include "../Include/Motor.h"
 #include "../Include/LimitSwitch.h"
 #include "DataBaseUnitTest.h"
@@ -34,45 +35,25 @@ void printColum(vector<vector<string>> list){
 		cout << str[1] << endl;
 	}
 }
-void setIngredientPositions(Data *myData, SystemConfig *sys, vector<string> const *ingredients){
-	int index = 0;
-	for(auto& ing: *ingredients){
-		int temp_ingredient_idx = myData->ingredientToIndex(ing);
-		sys->updatePosition(index, temp_ingredient_idx);
-		// cout << index << "\t" << temp_ingredient_idx << "\t" << ing << endl;
-		index ++;
-	}
 
-}
-void printSys(SystemConfig sys){
-	for(auto& ing: sys.positions){
-		cout << ing.getIngredient() << "\t" << ing.getPosition() << endl;
-		// int a = 2, b = 4;
-		// ing.setIngredient(a);
-		// ing.setPosition(b);
-		// cout << ing.ingredient << "\t" << ing.position << endl;
-
-	}
-}
-
-vector<int> DataBaseExecutable(){
+int DataBaseExecutable(){
     Data* myData = new Data();
-	SystemConfig* sys = new SystemConfig();
-	Motor* myMotor = new Motor(1,2,3,4, 1.8);
+
 
 	vector<vector<string>> df = myData->df_menu;
 	vector<vector<string>> ing_df = myData->ingredient_indexes.df;
 	vector<string> ing = myData->ingredient_indexes.ingredients;
 	vector<string> idx = myData->ingredient_indexes.indexes;
-
-	vector<string> system_ingredients = {"Vodka","Gin", "Cointreau", "Cranberry Juice", "Mezcal","Lime Juice"};
-	setIngredientPositions(myData, sys, &system_ingredients);
-
-	// printSys(*sys);
+	vector<vector<string>> fill_df = myData->fill_data;
 
 	string desired_drink = "Cosmopolitan";
-	vector<int> drink_queue = myData->getRecipe(desired_drink, *sys);
+	vector<string> drinkList = myData->getActiveDrinkList();
+	vector<int> drink_queue = myData->getRecipe(desired_drink);
+	printList(drinkList);
+	printListOfInts(drink_queue);
+	// cout << "HELLO" <<endl;
 
-	vector<int> step_queue = myMotor->getStepQueue(drink_queue);
-	return step_queue;
+
+	
+	return 0;
 }
